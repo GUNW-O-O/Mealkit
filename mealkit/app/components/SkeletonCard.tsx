e546194a15d577fc4d332f19e0ac5a1d5d02b688
GUNW-O-O <rhrjsdn12@naver.com>
2026-01-15 15:56:09 +0900

    ci 테스트

--- mealkit/app/components/SkeletonCard.tsx
+++ mealkit/app/components/SkeletonCard.tsx
@@ -1,6 +1,6 @@
 export default function SkeletonCard() {
   return (
-    <div className="relative w-full aspect-square overflow-hidden rounded bg-gray-200">
+    <div className="relative w-full aspect-square overflow-hidden rounded bg-gray-400">
       <div className="absolute inset-0 skeleton-shimmer" />
     </div>
   )
