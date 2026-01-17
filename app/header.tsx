4a46c31c48e5b6cecf8986e746cc8d4a4c963a64
GUNW-O-O <rhrjsdn12@naver.com>
2026-01-17 19:25:24 +0900

    장바구니 기능 추가 - 삭제 수정 Todo

--- app/header.tsx
+++ app/header.tsx
@@ -1,25 +1,49 @@
 'use client'
 
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 export default function Header() {
   const [theme, setTheme] = useState<'light' | 'dark'>('light')
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     document.documentElement.classList.toggle('dark', theme === 'dark')
   }, [theme])
 
+  useEffect(() => {
+    const fetchCount = () => {
+      fetch('/api/cart/count')
+        .then(res => res.json())
+        .then(data => setCount(data.count))
+    }
+
+    // 마운트시
+    fetchCount()
+
+    window.addEventListener('cart-updated', fetchCount)
+    return () => window.removeEventListener('cart-updated', fetchCount)
+  }, [])
+
+
   return (
     <header className="sticky top-0 z-10 bg-emerald-400 text-gray-900">
       <div className="max-w-6xl mx-auto px-4 h-14 flex items-center justify-between">
-        <h1 className="text-base font-bold">척척밥상</h1>
+        <Link href="/" className="text-base font-bold">척척밥상</Link>
 
-        <button
-          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-          className="text-sm border border-white/50 rounded px-3 py-1 hover:bg-white/10"
-        >
-          {theme === 'light' ? '🌙' : '☀️'}
-        </button>
+        <div className='flex'>
+          <Link href="/cart">
+            <button className="text-sm mr-3 bg-white/80 border border-white/50 rounded px-3 py-1 hover:bg-white/70">
+              🛒 {count}
+            </button>
+          </Link>
+          <button
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            className="text-sm border border-white/50 rounded px-3 py-1 hover:bg-white/10"
+          >
+            {theme === 'light' ? '🌙' : '☀️'}
+          </button>
+        </div>
       </div>
     </header>
   )
--- app/types/product.ts
+++ app/types/product.ts
@@ -7,6 +7,15 @@ export interface Product {
   image : string | null
 }
 
+export interface RegProduct {
+  index : number
+  name : string
+  price : number
+  current : number
+  limit : number
+  image : string | null
+}
+
 export interface ProductResponse {
   status : number
   content : Product[]
